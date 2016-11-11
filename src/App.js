b571ce387d1e7505ba6a4f951b01384f416f6a3b
lglomer <lglomer@gmail.com>
2016-11-11 16:57:22 +0200

    basic redux init, eslint fixed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View />
+      </Provider>
+    );
+  }
+}
+
+export default App;
