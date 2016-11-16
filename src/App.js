e28a6128399f7e9e3b579b11a0ca1780b259b037
lglomer <lglomer@gmail.com>
2016-11-16 20:31:03 +0200

    Styling the navigator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 /* eslint-disable  global-require */
-import {
-  Platform
-} from 'react-native';
+//import { Platform } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
 
 registerScreens(); // this is where you register all of your app's screens
-
-// Navigation.startTabBasedApp({
-//   tabs: [
-//     {
-//       label: 'Feed',
-//       screen: 'PetSpot.Feed', // this is a registered name for a screen
-//       icon: require('../img/one@1x.android.png'),
-//       title: 'Feed'
-//     },
-//     {
-//       label: 'Login',
-//       screen: 'PetSpot.Login',
-//       icon: require('../img/two@2x.png'),
-//       selectedIcon: require('../img/two_selected@2x.png'),
-//       title: 'Login'
-//     }
-//   ]
-// });
-
 
 const createTabs = () => {
   const tabs = [
@@ -33,28 +12,32 @@ const createTabs = () => {
       screen: 'PetSpot.Feed',
       icon: require('../img/one@1x.android.png'),
       selectedIcon: require('../img/one_selected@2x.png'),
-      title: 'Screen One'
+      title: 'Feed'
     },
     {
       label: 'Login',
       screen: 'PetSpot.Login',
       icon: require('../img/two@2x.png'),
       selectedIcon: require('../img/two_selected@2x.png'),
-      title: 'Screen Two',
-      navigatorStyle: {
-        tabBarBackgroundColor: '#4dbce9',
-      }
+      title: 'Login',
+    },
+    {
+      label: 'Collapse',
+      screen: 'PetSpot.CollapsingTopBarScreen',
+      icon: require('../img/three@2x.png'),
+      selectedIcon: require('../img/three_selected@2x.png'),
+      title: 'Collapse Bro',
     }
   ];
 
-  if (Platform.OS === 'android') {
-    tabs.push({
-      label: 'Collapsing',
-      screen: 'PetSpot.CollapsingTopBarScreen',
-      icon: require('../img/one@1x.android.png'),
-      title: 'Collapsing',
-    });
-  }
+  // if (Platform.OS === 'android') {
+  //   tabs.push({
+  //     label: 'Collapsing',
+  //     screen: 'PetSpot.CollapsingTopBarScreen',
+  //     icon: require('../img/one@1x.android.png'),
+  //     title: 'Huh',
+  //   });
+  // }
   return tabs;
 };
 
@@ -62,9 +45,9 @@ const createTabs = () => {
 Navigation.startTabBasedApp({
   tabs: createTabs(),
   appStyle: {
-    tabBarBackgroundColor: '#0f2362',
-    tabBarButtonColor: '#ffffff',
-    tabBarSelectedButtonColor: '#63d7cc'
+    tabBarBackgroundColor: '#ffffff',
+    tabBarButtonColor: '#444444',
+    tabBarSelectedButtonColor: '#42A5F5'
   },
   drawer: {
     left: {
--- src/screens/SideMenu.js
+++ src/screens/SideMenu.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import {
 	View,
-	Text,
+	Text
 } from 'react-native';
 
 class SideMenu extends Component {
 	render() {
+		const { menuContainer, menuHeader, menuBody } = styles;
 		return (
-      <View>
-        <Text>SideMenu BRO</Text>
-      </View>
+      <View style={menuContainer}>
+				<View style={menuHeader} >
+					<Text>Cool stuff goes around here</Text>
+				</View>
+				<View style={menuBody} >
+					<Text>Rock your body</Text>
+				</View>
+			</View>
 		);
 	}
 }
+
+const styles = {
+	menuContainer: {
+		backgroundColor: '#ffffff',
+		flexDirection: 'column',
+		flex: 1
+	},
+	menuHeader: {
+		backgroundColor: 'steelblue',
+		flex: 3
+	},
+	menuBody: {
+		backgroundColor: '#ffffff',
+		flex: 7
+	},
+};
 
 export default SideMenu;
